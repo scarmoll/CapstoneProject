840381f26695cd65b3d37cd23accf828c6b48f36
Oscar Mollé <scarmoll@gmail.com>
2021-01-03 14:17:52 +0100

    navigation working, changed names for consistency

--- app/src/main/java/com/example/capstone/MainActivity.kt
+++ app/src/main/java/com/example/capstone/MainActivity.kt
@@ -1,22 +1,65 @@
 package com.example.capstone
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        navController = findNavController(R.id.nav_host_fragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_1 -> {
+                    navController.navigate(
+                        R.id.PartsFragment
+                    )
+                    true
+                }
+
+                R.id.page_2 -> {
+                    true
+                }
+
+                R.id.page_3 -> {
+                    true
+                }
+
+                else -> false
+            }
+        }
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
+        }
+
+        destinationListener()
+    }
+
+    private fun destinationListener() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in arrayOf(R.id.WelcomeFragment)) {
+                toolbar.title = "Welkom"
+                bottom_navigation.setVisibility(View.GONE)
+            } else if (destination.id in arrayOf(R.id.PartsFragment)) {
+                toolbar.title = "Selecteer Onderdelen"
+                bottom_navigation.setVisibility(View.VISIBLE)
+            }
         }
     }
 
