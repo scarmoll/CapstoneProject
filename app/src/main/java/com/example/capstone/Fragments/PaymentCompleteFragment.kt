54e23b857675279e15c46fb546e593be1e08ab92
Oscar Mollé <scarmoll@gmail.com>
2021-01-03 21:31:06 +0100

    added paymetcomplete fragment

--- app/src/main/java/com/example/capstone/Fragments/PaymentCompleteFragment.kt
+++ app/src/main/java/com/example/capstone/Fragments/PaymentCompleteFragment.kt
@@ -5,26 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.example.capstone.R
-import kotlinx.android.synthetic.main.fragment_payment.*
+import kotlinx.android.synthetic.main.fragment_payment_complete.*
 
-class PaymentFragment : Fragment() {
+class PaymentCompleteFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payment, container, false)
+        return inflater.inflate(R.layout.fragment_payment_complete, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        paymentFAB.setOnClickListener {
-            view.findNavController().navigate(R.id.PaymentCompleteFragment)
+        btnReturnHome.setOnClickListener {
+            view.findNavController().navigate(R.id.PartsFragment)
+
         }
+
         super.onViewCreated(view, savedInstanceState)
     }
 }
