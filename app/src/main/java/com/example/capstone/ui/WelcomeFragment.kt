2cbfc92b7d2afd914abb2d8815f1fe7fb105946e
Oscar Mollé <scarmoll@gmail.com>
2021-01-18 11:26:31 +0100

    app now only shows welcome fragment once

--- app/src/main/java/com/example/capstone/ui/WelcomeFragment.kt
+++ app/src/main/java/com/example/capstone/ui/WelcomeFragment.kt
@@ -1,13 +1,15 @@
 package com.example.capstone.ui
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.capstone.R
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -15,8 +17,8 @@ import com.example.capstone.R
 class WelcomeFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_welcome, container, false)
@@ -24,6 +26,17 @@ class WelcomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val sharedPref = context?.getSharedPreferences("PREF", Context.MODE_PRIVATE)
+
+        // Wipes shared preferences, for demo purposes
+//        sharedPref?.edit()?.clear()?.apply()
+
+        if (sharedPref?.getBoolean("prevStart", false)!!) {
+            findNavController().navigate(R.id.action_WelcomeFragment_to_PartsFragment)
+        } else {
+            sharedPref.edit()?.putBoolean("prevStart", true)?.apply()
+        }
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_WelcomeFragment_to_PartsFragment)
